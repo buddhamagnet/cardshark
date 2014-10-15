e3eeeab3ab86b7387ad7bc570af9a188fb275378
buddhamagnet <buddhamagnet@gmail.com>
2014-10-15 06:44:26 -0400

    add first unit test for card names

--- test/models/card_test.rb
+++ test/models/card_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @suit = Suit.new(name: 'hearts')
+    @card = Card.create(identifier: "king")
+    @suit.cards << @card
+    @suit.save
+  end
+
+  def test_friendly_name
+    assert_equal("king of hearts", @card.to_s)
+  end
 end
