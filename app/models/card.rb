9517242840a9f0038449df976266d6ddd8906b90
buddhamagnet <buddhamagnet@gmail.com>
2014-10-15 10:20:31 -0400

    delegate suit name to suit model

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,15 +1,17 @@
 class Card < ActiveRecord::Base
   belongs_to :suit
 
+  delegate :name, :to => :suit, :prefix => true
+
   def orphaned?
     suit.nil?
   end
 
   def to_s
-    orphaned? ? identifier : "#{identifier} of #{suit.name}"
+    orphaned? ? identifier : "#{identifier} of #{suit_name}"
   end
 
   def icon
-    orphaned? ? "#{identifier}.png" : "#{identifier}_#{suit.name}.png"
+    orphaned? ? "#{identifier}.png" : "#{identifier}_#{suit_name}.png"
   end
 end
