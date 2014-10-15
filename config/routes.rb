1092fcb7827fae8c4a39aa40e8300dc71c0e6293
buddhamagnet <buddhamagnet@gmail.com>
2014-10-15 08:13:00 -0400

    add shuffle action on collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'cards/index'
   root 'cards#index'
+
+  resources :cards, only: [:index] do
+    collection do
+      get 'shuffle', as: :shuffle
+    end
+  end
 end
