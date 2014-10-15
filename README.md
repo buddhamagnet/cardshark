d9ee8224ee339207dc1807b5fc453017f2ca53e1
buddhamagnet <buddhamagnet@gmail.com>
2014-10-15 07:34:20 -0400

    add README and rework seeds file

---
+++ README.md
@@ -0,0 +1,10 @@
+A super simple card shuffling app provided as part of a technical test.
+
+Build time: 40 minutes.
+
+INSTALLATION:
+
+1. Run bin/rake db:seed.
+2. Load the application.
+3. Hit the shuffle link.
+4. Rinse and repeat.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,20 @@
+Suit.destroy_all
+
+Card.where(identifier: "joker").first.delete
+
 Suit.create(name: 'hearts')
 Suit.create(name: 'spades')
 Suit.create(name: 'clubs')
 Suit.create(name: 'diamonds')
 
 Suit.all.each do |suit|
-  1.upto(10) do |i|
+  2.upto(10) do |i|
     Card.create(identifier: i, suit: suit)
   end
+  Card.create(identifier: 'ace', suit: suit)
   Card.create(identifier: 'jack', suit: suit)
   Card.create(identifier: 'queen', suit: suit)
   Card.create(identifier: 'king', suit: suit)
 end
+
+Card.create(identifier: 'joker')
