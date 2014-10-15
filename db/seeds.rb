1563b7a8297d53b0a8265ecf7d9643e3d4edb6a3
buddhamagnet <buddhamagnet@gmail.com>
2014-10-15 08:07:18 -0400

    create cards in unshuffled order

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,11 @@ Suit.create(name: 'clubs')
 Suit.create(name: 'diamonds')
 
 Suit.all.each do |suit|
+  Card.create(identifier: 'ace', suit: suit)
+
   2.upto(10) do |i|
     Card.create(identifier: i, suit: suit)
   end
-  Card.create(identifier: 'ace', suit: suit)
   Card.create(identifier: 'jack', suit: suit)
   Card.create(identifier: 'queen', suit: suit)
   Card.create(identifier: 'king', suit: suit)
